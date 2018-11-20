dc83b0705ba5ab0800ced2a79a324335111ba0e0
Chase Moore <chlmr87@gmail.com>
2018-11-20 07:09:15 -0500

    modification

--- Solutions/src/main/java/cm/stealthwatch/problems/Solutions/gnode/GraphNode.java
+++ Solutions/src/main/java/cm/stealthwatch/problems/Solutions/gnode/GraphNode.java
@@ -1,7 +1,83 @@
 package cm.stealthwatch.problems.Solutions.gnode;
 
-public class GraphNode {
-	GNodeImpl gnode = new GNodeImpl("A", null);
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+
+public class GraphNode implements GNode{
+
+	private final GNode[] NO_CHILDREN = {};
+	String name;
+	GNode[] children;
+
+
+	public GraphNode(String name) {
+		this.name = name;
+	}
 	
-	String name = gnode.getName();
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	public void addChild(GNode gnode) {
+		if(null == this.children)
+			this.children = new GNode[0];
+		List<GNode> nodes = Arrays.asList(this.children);
+		nodes = new ArrayList<GNode>(nodes);
+		nodes.add(gnode);
+		GNode[] children = new GNode[nodes.size()];
+		this.children = nodes.toArray(children);
+	}
+
+	@Override
+	public GNode[] getChildren() {
+		if(null == children || children.length == 0)
+			return NO_CHILDREN;
+		return children;
+	}
+	
+	public static ArrayList<String> walkGraph(GNode node) {
+		Queue<GNode> queue = new LinkedList<>();
+		Set<String> nodes = new LinkedHashSet<>();
+		
+		queue.add(node);
+		while(!queue.isEmpty()) {
+			nodes.add(queue.peek().getName());
+			queue.addAll(Arrays.asList(queue.remove().getChildren()));
+		}
+		
+		return new ArrayList<String>(nodes);
+	}
+	
+	public static ArrayList<ArrayList<String>> printPaths(GNode node) {
+		List<ArrayList<String>> paths = new ArrayList<ArrayList<String>>();
+		List<String> currentPath = new ArrayList<String>();
+		Queue<GNode> queue = new LinkedList<>();
+		Set<GNode> visited = new HashSet<>();
+		
+		queue.add(node);
+		while(!queue.isEmpty()) {
+			currentPath.add(queue.peek().getName());
+			List<GNode> childrenList = Arrays.asList(queue.peek().getChildren());
+			for(GNode gNode:childrenList) {
+
+			}
+		}
+		
+		//from the starting node add to the current path list and get the children
+		//go down to the first child add to the current path list and get the children 
+		//go further until no more children obtained and add this to a visited list
+		//add current path to the paths list
+		//
+		return (ArrayList<ArrayList<String>>) paths;
+	}
+
 }
--- Solutions/src/test/java/cm/stealthwatch/problems/Solutions/GNodeTest.java
+++ Solutions/src/test/java/cm/stealthwatch/problems/Solutions/GNodeTest.java
@@ -7,15 +7,14 @@ import java.util.logging.Logger;
 
 import org.junit.Test;
 
-import cm.stealthwatch.problems.Solutions.gnode.GNode;
-import cm.stealthwatch.problems.Solutions.gnode.GNodeImpl;
+import cm.stealthwatch.problems.Solutions.gnode.GraphNode;
 
 
 
 public class GNodeTest {
 	private static final Logger LOGGER = Logger.getLogger( GNodeTest.class.getName());
 
-	GNodeImpl gNodeImpl = new GNodeImpl("A", null);
+	GraphNode gNodeImpl = new GraphNode("A");
 	
 	@Test
 	public void singleNodeTest() {
@@ -25,13 +24,19 @@ public class GNodeTest {
 	
 	@Test
 	public void childrenNodeTest() {
-		GNodeImpl gNodeE = new GNodeImpl("E", null);
-		GNodeImpl gNodeD = new GNodeImpl("D", null);
-		GNodeImpl gNodeC = new GNodeImpl("C", new GNode[] {gNodeE});
-		GNodeImpl gNodeB = new GNodeImpl("B", new GNode[] {gNodeE});
-		GNodeImpl gNodeA = new GNodeImpl("A", new GNode[] {gNodeC});
+		GraphNode gNodeE = new GraphNode("E");
+		GraphNode gNodeD = new GraphNode("D");
+		GraphNode gNodeC = new GraphNode("C");
+		GraphNode gNodeB = new GraphNode("B");
+		GraphNode gNodeA = new GraphNode("A");
 		
-		LOGGER.info(GNodeImpl.walkGraph(gNodeA).toString());
+
+		gNodeA.addChild(gNodeD);
+		gNodeD.addChild(new GraphNode("Z"));
+		gNodeD.addChild(gNodeC);
+		gNodeA.addChild(gNodeB);
+		
+		LOGGER.info(GraphNode.walkGraph(gNodeA).toString());
 		
 		
 	}
