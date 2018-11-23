646dd2c76699a2686e89ed463ec6eb96db9a682d
VirgilHawkins <chase.moore2010@gmail.com>
2018-11-22 22:18:54 -0500

    Corrected paths method to get all paths of a graph
    Print Paths was only returning paths for a predetermined set in the example code. I have extended this to cover N number of nodes and depths.

--- Solutions/src/main/java/cm/stealthwatch/problems/Solutions/gnode/GraphNode.java
+++ Solutions/src/main/java/cm/stealthwatch/problems/Solutions/gnode/GraphNode.java
@@ -3,17 +3,14 @@ package cm.stealthwatch.problems.Solutions.gnode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Deque;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Set;
-import java.util.logging.Logger;
 
 
 public class GraphNode implements GNode {
-	private static final Logger LOGGER = Logger.getLogger(GraphNode.class.getName());
 
 	private final GNode[] NO_CHILDREN = {};
 	String name;
@@ -82,7 +79,6 @@ public class GraphNode implements GNode {
 	public static ArrayList<ArrayList<String>> printPaths(GNode node) {
 		List<ArrayList<String>> paths = new ArrayList<ArrayList<String>>();
 		ArrayList<String> currentPath = new ArrayList<String>();
-		List<String> continuePath = new ArrayList<>();
 		Deque<GNode> childQueue = new LinkedList<>();
 		Deque<GNode> parentQueue = new LinkedList<>();
 		Set<GNode> visited = new LinkedHashSet<>();
@@ -97,27 +93,28 @@ public class GraphNode implements GNode {
 		 */
 		parentQueue.add(node);
 		while(!parentQueue.isEmpty()) {
-			if(nodesAreVisited(parentQueue.getLast(), visited)) {
+			while(nodesAreVisited(parentQueue.getLast(), visited)) {
 				visited.add(parentQueue.getLast());
 				currentPath.remove(parentQueue.getLast().getName());
 				parentQueue.removeLast();
-				continue;
+				if(parentQueue.isEmpty())
+					break;
 			} 
+			
+			if(parentQueue.isEmpty())
+				continue;
 			
 			if(parentQueue.getLast().getChildren().length > 0 && !extracted.contains(parentQueue.getLast())) {
 				childQueue.addAll(Arrays.asList(parentQueue.getLast().getChildren()));
 				currentPath.add(parentQueue.getLast().getName());
+				extracted.add(parentQueue.getLast());
 			} else if (parentQueue.getLast().getChildren().length == 0){
 				currentPath.add(parentQueue.getLast().getName());
 				paths.add(currentPath);
 				parentQueue.removeLast();
 				continue;
 			}
-			
-			if(nodesAreVisited(parentQueue.getLast(), visited)) {
-				
-			}
-			
+
 			if(childQueue.getLast().getChildren().length > 0) {
 				parentQueue.addLast(childQueue.removeLast());
 				continue;
@@ -129,10 +126,13 @@ public class GraphNode implements GNode {
 					currentPath = new ArrayList<>(currentPath);
 					currentPath.remove(childQueue.getLast().getName());
 					childQueue.removeLast();
+					if(childQueue.isEmpty())
+						break;
 					if(childQueue.getLast().getChildren().length > 0) {
 						parentQueue.addLast(childQueue.removeLast());
 						break;
-					}
+					} 
+					
 					if(visited.containsAll(Arrays.asList(parentQueue.getLast().getChildren()))) {
 						currentPath.remove(parentQueue.getLast().getName());
 						visited.add(parentQueue.removeLast());
@@ -151,41 +151,35 @@ public class GraphNode implements GNode {
 	}
 
 	public static void main(String args[]) {
-		GraphNode A = new GraphNode("A");
-		GraphNode B = new GraphNode("B");
-		GraphNode C = new GraphNode("C");
-		GraphNode D = new GraphNode("D");
-		GraphNode E = new GraphNode("E");
-		GraphNode F = new GraphNode("F");
-		GraphNode G = new GraphNode("G");
-		GraphNode H = new GraphNode("H");
-		GraphNode I = new GraphNode("I");
-		GraphNode J = new GraphNode("J");
-		GraphNode K = new GraphNode("K");
-		GraphNode L = new GraphNode("L");
-		GraphNode M = new GraphNode("M");
-		A.addChild(B);
-		A.addChild(C);
-		A.addChild(D);
-		B.addChild(E);
-		B.addChild(F);
-		B.addChild(G);
-		E.addChild(H);
-		E.addChild(I);
-		E.addChild(J);
-		J.addChild(K);
-		K.addChild(L);
-		K.addChild(M);
+		GraphNode gNodeA = new GraphNode("A");
+		GraphNode gNodeB = new GraphNode("B");
+		GraphNode gNodeC = new GraphNode("C");
+		GraphNode gNodeD = new GraphNode("D");
+		GraphNode gNodeE = new GraphNode("E");
+		GraphNode gNodeF = new GraphNode("F");
+		GraphNode gNodeG = new GraphNode("G");
+		GraphNode gNodeH = new GraphNode("H");
+		GraphNode gNodeI = new GraphNode("I");
+		GraphNode gNodeJ = new GraphNode("J");
+		GraphNode gNodeK = new GraphNode("K");
+		GraphNode gNodeL = new GraphNode("L");
+		GraphNode gNodeM = new GraphNode("M");
+		gNodeA.addChild(gNodeB);
+		gNodeA.addChild(gNodeC);
+		gNodeA.addChild(gNodeD);
+		gNodeB.addChild(gNodeE);
+		gNodeB.addChild(gNodeF);
+		gNodeB.addChild(gNodeG);
+		gNodeE.addChild(gNodeH);
+		gNodeE.addChild(gNodeI);
+		gNodeE.addChild(gNodeJ);
+		gNodeJ.addChild(gNodeK);
+		gNodeK.addChild(gNodeL);
+		gNodeK.addChild(gNodeM);
 		
 
-		System.out.println(printPaths(A));
-		
-//		LOGGER.info("Walking a whole graph: "+walkGraph(gNodeA));
-//		LOGGER.info("Walking a partial graph: "+walkGraph(gNodeB));
-//		LOGGER.info("Single node: "+walkGraph(new GraphNode("Z")));
-
-//		LOGGER.info("Paths of a part graph: " + printPaths(gNodeC));
-//		LOGGER.info("Testing of a single node: "+printPaths(new GraphNode("Z")));
+		System.out.println(walkGraph(gNodeA));
+		System.out.println(printPaths(gNodeA));
 
 	}
 
