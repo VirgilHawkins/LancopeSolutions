0ebda995c5159d7dc4128d812a69199f7d0cdb84
VirgilHawkins <chase.moore2010@gmail.com>
2018-11-22 22:26:18 -0500

    Updating test to reflect print path changes
    Made changes to print path so I updated the associated test

--- Solutions/src/test/java/cm/stealthwatch/problems/Solutions/GraphNodeTest.java
+++ Solutions/src/test/java/cm/stealthwatch/problems/Solutions/GraphNodeTest.java
@@ -48,26 +48,35 @@ public class GraphNodeTest {
 	
 	@Test
 	public void printPathTest() {
-		GraphNode gNodeD = new GraphNode("D");
-		GraphNode gNodeC = new GraphNode("C");
-		GraphNode gNodeB = new GraphNode("B");
 		GraphNode gNodeA = new GraphNode("A");
-
+		GraphNode gNodeB = new GraphNode("B");
+		GraphNode gNodeC = new GraphNode("C");
+		GraphNode gNodeD = new GraphNode("D");
+		GraphNode gNodeE = new GraphNode("E");
+		GraphNode gNodeF = new GraphNode("F");
+		GraphNode gNodeG = new GraphNode("G");
+		GraphNode gNodeH = new GraphNode("H");
+		GraphNode gNodeI = new GraphNode("I");
+		GraphNode gNodeJ = new GraphNode("J");
+		GraphNode gNodeK = new GraphNode("K");
+		GraphNode gNodeL = new GraphNode("L");
+		GraphNode gNodeM = new GraphNode("M");
 		gNodeA.addChild(gNodeB);
-		gNodeB.addChild(new GraphNode("E"));
-		gNodeB.addChild(new GraphNode("F"));
-		gNodeC.addChild(new GraphNode("G"));
-		gNodeC.addChild(new GraphNode("H"));
-		gNodeC.addChild(new GraphNode("I"));
-		gNodeD.addChild(new GraphNode("J"));
 		gNodeA.addChild(gNodeC);
 		gNodeA.addChild(gNodeD);
+		gNodeB.addChild(gNodeE);
+		gNodeB.addChild(gNodeF);
+		gNodeB.addChild(gNodeG);
+		gNodeE.addChild(gNodeH);
+		gNodeE.addChild(gNodeI);
+		gNodeE.addChild(gNodeJ);
+		gNodeJ.addChild(gNodeK);
+		gNodeK.addChild(gNodeL);
+		gNodeK.addChild(gNodeM);
 		
 		List<ArrayList<String>> paths = GraphNode.printPaths(gNodeA);
-		assertTrue(paths.contains(Arrays.asList(new String[] {"A","D","J"})));
-		assertTrue(paths.contains(Arrays.asList(new String[] {"A","C","I"})));
-		assertTrue(paths.contains(Arrays.asList(new String[] {"A","B","E"})));
-
+		assertTrue(paths.contains(Arrays.asList(new String[] {"A","D"})));
+		assertTrue(paths.contains(Arrays.asList(new String[] {"A","C"})));
+		assertTrue(paths.contains(Arrays.asList(new String[] {"A","B","F",})));
 	}
-	
 }
