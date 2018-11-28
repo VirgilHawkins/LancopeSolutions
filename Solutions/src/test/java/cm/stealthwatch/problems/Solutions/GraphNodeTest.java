ed1491ab264e984a309f13f332f28a16bbcfefdd
VirgilHawkins <chase.moore2010@gmail.com>
2018-11-28 11:39:28 -0500

    Updating printPaths test to reflect example
    Captured all possible scenarios

--- Solutions/src/test/java/cm/stealthwatch/problems/Solutions/GraphNodeTest.java
+++ Solutions/src/test/java/cm/stealthwatch/problems/Solutions/GraphNodeTest.java
@@ -58,25 +58,24 @@ public class GraphNodeTest {
 		GraphNode gNodeH = new GraphNode("H");
 		GraphNode gNodeI = new GraphNode("I");
 		GraphNode gNodeJ = new GraphNode("J");
-		GraphNode gNodeK = new GraphNode("K");
-		GraphNode gNodeL = new GraphNode("L");
-		GraphNode gNodeM = new GraphNode("M");
 		gNodeA.addChild(gNodeB);
 		gNodeA.addChild(gNodeC);
 		gNodeA.addChild(gNodeD);
 		gNodeB.addChild(gNodeE);
 		gNodeB.addChild(gNodeF);
-		gNodeB.addChild(gNodeG);
-		gNodeE.addChild(gNodeH);
-		gNodeE.addChild(gNodeI);
-		gNodeE.addChild(gNodeJ);
-		gNodeJ.addChild(gNodeK);
-		gNodeK.addChild(gNodeL);
-		gNodeK.addChild(gNodeM);
+		gNodeC.addChild(gNodeG);
+		gNodeC.addChild(gNodeH);
+		gNodeC.addChild(gNodeI);
+		gNodeD.addChild(gNodeJ);
 		
 		List<ArrayList<String>> paths = GraphNode.printPaths(gNodeA);
-		assertTrue(paths.contains(Arrays.asList(new String[] {"A","D"})));
-		assertTrue(paths.contains(Arrays.asList(new String[] {"A","C"})));
+		System.out.println(paths);
+		assertTrue(paths.contains(Arrays.asList(new String[] {"A","D","J"})));
+		assertTrue(paths.contains(Arrays.asList(new String[] {"A","C","I"})));
+		assertTrue(paths.contains(Arrays.asList(new String[] {"A","C","H"})));
+		assertTrue(paths.contains(Arrays.asList(new String[] {"A","C","G"})));
 		assertTrue(paths.contains(Arrays.asList(new String[] {"A","B","F",})));
+		assertTrue(paths.contains(Arrays.asList(new String[] {"A","B","E",})));
+
 	}
 }
